3b26265c4d8786cee4fac295f53ca71ea57630d1
Daniel C. Elton <delton17@gmail.com>
2018-05-31 18:17:50 -0400

    update training regimine

--- model/test.py
+++ model/test.py
@@ -11,12 +11,13 @@ params = {
     'DIS_FILTER_SIZES': [5, 10, 15],
     'DIS_NUM_FILTERS': [100, 100, 100],
     'DIS_DROPOUT'              :   0.85,
-    'DIS_L2REG' :   0.3
+    'DIS_L2REG' :   0.3,
+  # 'CHK_PATH' : '/home/delton/Dropbox/AAA_UMD_RESEARCH/ORGANIC/model/test_ckpt'
 }
 
-model = ORGANIC('tutorial1', params=params)
+model = ORGANIC('energetics_test2', params=params)
 #model.load_training_set('../data/trainingsets/small_mols.csv')
 model.load_training_set('../data/datasets/energetics_list.csv')
-model.set_training_program(['logP'], [50])
+model.set_training_program(['diversity',  'variety','soft_novelty'], [50,50,50] )
 model.load_metrics()
 model.train(ckpt_dir='test_ckpt')
